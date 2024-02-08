9b0bcf124f1937a308b30662d8629f2922335b68
Annateresa01 <shajianna013@gmail.com>
2024-02-08 22:29:31 +0530

    ok

--- controllers/petrouter.js
+++ controllers/petrouter.js
@@ -6,13 +6,18 @@ const router=express.Router()
 router.post("/add",async(req,res)=>{
 let data=req.body
 let pets=new petmodel(data)
-let res=await pets.save()
+let result=await pets.save()
 res.json(
     {
         status:"Success"
     }
 )
 
+})
+
+router.get("/view",async(req,res)=>{
+    let data=await petmodel.find()
+    res.json(data)
 })
 
 module.exports=router
